cba0fdfc72c25c9b2c608765168f4b8087f70df7
Rahul Singh <rarr82792@gmail.com>
2025-11-03 12:13:40 +0530

    Test CI/CD trigger

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Welcome to Django Todo App Home!</h1>")
+    return HttpResponse("<h1>Welcome to Django Todo App Home!and this is so exciting</h1>")
 
