231b27b3925aeabfe0ec42040f62335eea0e1e72
rsrr82792-glitch <rsrr82792@gmail.com>
2025-11-05 11:01:33 +0000

    fixed ownership and git identity issue

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ubuntu/django-todoapp/venv
+set -gx VIRTUAL_ENV /var/lib/jenkins/workspace/django-todoapp-pipeline/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
